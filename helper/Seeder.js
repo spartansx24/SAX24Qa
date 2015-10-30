0ee159901c32b4f7532036a5ba2f4d856264e8ab
sandesh-extentia <sandesh.magdum@extentia.com>
2015-10-30 23:37:32 +0530

    - Added route for sponsors list by radius.

--- helper/Seeder.js
+++ helper/Seeder.js
@@ -1,7 +1,7 @@
 var restful = require('node-restful');
 var async = require('async');
 var mongoose = restful.mongoose;
-var roleModel = require('../model/role');
+var userModel = require('../model/users');
 var CONSTANT = require('../utilities/Constant');
 
 function PopulateDB(){
@@ -14,16 +14,155 @@ PopulateDB.prototype.writeToDB = function(){
 
 		var locals = {};
 
-		var roles = [{"Id":1,"Name":"sponser"}, {"Id":2,"Name":"ngo"}, {"Id":3,"Name":"user"}];
+		var sponsors = [{
+				"firstName": "Binu",
+				"lastName": "Moothedan",
+				"dob": "1980-10-28T09:16:33.823Z",
+				"gender": "Male",
+				"weight": "65 kgs",
+				"height": "5 ft. 9 inches",
+				"email": "binu.moothedan@extentia.com",
+				"password": "demo",
+				"mobile": "9096629481",
+				"bandId": "band1",
+				"bandDetails": "testband",
+				"userRole": 1
+			},
+			{
+				"firstName": "Sandesh",
+				"lastName": "Magdum",
+				"dob": "1985-10-28T09:16:33.823Z",
+				"gender": "Male",
+				"weight": "72 kgs",
+				"height": "5 ft. 11 inches",
+				"email": "sandesh.magdum@extentia.com",
+				"password": "demo",
+				"mobile": "9096629481",
+				"bandId": "band2",
+				"bandDetails": "testband",
+				"userRole": 1
+			},
+			{
+				"firstName": "Nilesh",
+				"lastName": "Dethe",
+				"dob": "1985-10-28T09:16:33.823Z",
+				"gender": "Male",
+				"weight": "70 kgs",
+				"height": "5 ft. 10 inches",
+				"email": "nilesh.dethe@extentia.com",
+				"password": "demo",
+				"mobile": "9096629481",
+				"bandId": "band3",
+				"bandDetails": "testband",
+				"userRole": 1
+			},
+			{
+				"firstName": "Anil",
+				"lastName": "Metugade",
+				"dob": "1987-12-26T09:16:33.823Z",
+				"gender": "Male",
+				"weight": "67 kgs",
+				"height": "5 ft. 10 inches",
+				"email": "anil.metugade@extentia.com",
+				"password": "demo",
+				"mobile": "9096629481",
+				"bandId": "band4",
+				"bandDetails": "testband",
+				"userRole": 1
+			},
+			{
+				"firstName": "Saurabh",
+				"lastName": "Jadhav",
+				"dob": "1987-10-26T09:16:33.823Z",
+				"gender": "Male",
+				"weight": "70 kgs",
+				"height": "5 ft. 6 inches",
+				"email": "saurabh.jadhav@extentia.com",
+				"password": "demo",
+				"mobile": "9096629481",
+				"bandId": "band5",
+				"bandDetails": "testband",
+				"userRole": 1
+			},
+			{
+				"firstName": "Harpreet",
+				"lastName": "Singh",
+				"dob": "1983-10-25T09:16:33.823Z",
+				"gender": "Male",
+				"weight": "75 kgs",
+				"height": "5 ft. 7 inches",
+				"email": "harpreet.singh@extentia.com",
+				"password": "demo",
+				"mobile": "9096629481",
+				"bandId": "band6",
+				"bandDetails": "testband",
+				"userRole": 1
+			},
+			{
+				"firstName": "Netaji",
+				"lastName": "Fegade",
+				"dob": "1983-10-25T09:16:33.823Z",
+				"gender": "Male",
+				"weight": "70 kgs",
+				"height": "5 ft. 10 inches",
+				"email": "netaji.fegade@extentia.com",
+				"password": "demo",
+				"mobile": "9096629481",
+				"bandId": "band7",
+				"bandDetails": "testband",
+				"userRole": 1
+			},
+			{
+				"firstName": "Arshad",
+				"lastName": "Tamboli",
+				"dob": "1988-10-25T09:16:33.823Z",
+				"gender": "Male",
+				"weight": "63 kgs",
+				"height": "5 ft. 10 inches",
+				"email": "arshad.tamboli@extentia.com",
+				"password": "demo",
+				"mobile": "9096629481",
+				"bandId": "band8",
+				"bandDetails": "testband",
+				"userRole": 1
+			},
+			{
+				"firstName": "Sangram",
+				"lastName": "Mohite",
+				"dob": "1985-10-25T09:16:33.823Z",
+				"gender": "Male",
+				"weight": "78 kgs",
+				"height": "6 ft. 00 inches",
+				"email": "sangram.mohite@extentia.com",
+				"password": "demo",
+				"mobile": "9096629481",
+				"bandId": "band9",
+				"bandDetails": "testband",
+				"userRole": 1
+			},
+			{
+				"firstName": "Sagar",
+				"lastName": "Vairagar",
+				"dob": "1985-10-25T09:16:33.823Z",
+				"gender": "Male",
+				"weight": "70 kgs",
+				"height": "5 ft. 09 inches",
+				"email": "sagar.vairagar@extentia.com",
+				"password": "demo",
+				"mobile": "9096629481",
+				"bandId": "band10",
+				"bandDetails": "testband",
+				"userRole": 1
+			}];
 		
 		async.series([function(callback){
-			roleModel.find({}, function(err, rolesInserted){
-				if(rolesInserted.length == 0){
+			userModel.find({}, function(err, sponsorsInserted){
+				if(sponsorsInserted.length == 0){
 					//locals[CONSTANT.TABLES.ROLE] = {};
-					async.forEach(roles, function(eachRole){
-						var roleModelObj = new roleModel(eachRole);
-						roleModelObj.save(function(err, role){
-							console.log('Roles inserted: ' + role._id);
+					async.forEach(sponsors, function(eachSponsor){
+						var sponsorsModelObj = new userModel(eachSponsor);
+						sponsorsModelObj.save(function(err, sponsor){
+							console.log('Runners inserted: ' + sponsor._id);
 							//locals[CONSTANT.TABLES.ROLE][admin.id] = admin._id;
 							//callback();
 						});
@@ -39,4 +178,4 @@ PopulateDB.prototype.writeToDB = function(){
 	});
 };
 
-module.exports = { 'PopulateDB': new PopulateDB() }
+module.exports = { 'PopulateDB': new PopulateDB() };
--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ var causesModel = require('./model/causes');
 var runerstepsModel = require('./model/runersteps');
 var runnerChallangesModel = require('./model/runnercausechallenges');
 var sponserChallangesModel = require('./model/sponsorchallenges');
+var CONSTANT = require('./utilities/Constant').CONSTANT;
 
 module.exports = function(app) {
     
@@ -90,6 +91,42 @@ module.exports = function(app) {
             }           
         });
     });
+
+    var distance = function (lat1, lng1, lat2, lng2) {
+        if (lat1.lat && lng1.lat) {
+            lat2 = lng1.lat;
+            lng2 = lng1.lng;
+            lng1 = lat1.lng;
+            lat1 = lat1.lat;
+        }
+
+        //Confirms the user inputted arguments are in the correct format
+        distanceArgumentCheck(lat1, lng1, lat2, lng2);
+        var R = 6371, // km
+            dLat = toRad(lat2-lat1),
+            dLng = toRad(lng2-lng1),
+            lat1 = toRad(lat1),
+            lat2 = toRad(lat2);
+
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.sin(dLng/2) * Math.sin(dLng/2) * Math.cos(lat1) * Math.cos(lat2),
+            c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)),
+            d = R * c; // Distance in k
+        return d;
+    };
+
+    app.get('/api/getSponsorsByRadius', function(req, res) {
+        userModel.find({userRole: 2}).lean().populate(CONSTANT.TABLES.SPONSOR_CHALLENGES).sort('-firstName').exec(function (err, sponsors) {
+            if (err) {
+               res.json({message:'Error in finding sponsors!'});
+            }
+            else {
+                // Sort by DeviceName, case-insensitive
+                res.json(sponsors);
+            }
+        });
+    });
+
 
     app.use('/', function(req, res) {        
         res.sendFile(__dirname + '/public/index.html');
