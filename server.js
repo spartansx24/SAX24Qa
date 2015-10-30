87cc2360689e5a778e53d09b3b73b77a5016d9e6
hsit18 <hsit18@gmail.com>
2015-10-30 20:08:32 +0530

    service added

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ mongoose.connection.on('open', function () {
     seeder.PopulateDB;
 });
 
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
