085168cca88349812b1de22a4deaf6a592c12f73
sandesh-extentia <sandesh.magdum@extentia.com>
2015-10-30 18:15:02 +0530

    - Added some more model names.

--- utilities/Constant.js
+++ utilities/Constant.js
@@ -2,11 +2,11 @@ module.exports = {
     'CONSTANT': {
         'TABLES': {
             'USERS': 'users',
-            'CAUSE': 'cause',
+            'CAUSES': 'cause',
             'RUNNER_STEPS': 'runner_steps',
-            'RUNNER_CHALLENGES': 'runner_challenges',
-            'SPONSORSHIP_LEVELS': 'sponsorship_levels',
-            'USER_ROLES': ['RUNNER': 1, 'SPONSOR': 2, 'NGO': 3]
+            'SPONSOR_CHALLENGES': 'sponsor_challenges',
+            'RUNNER_CAUSE_CHALLENGES': 'runner_cause_challenges',
+            'USER_ROLES': {'RUNNER': 1, 'SPONSOR': 2, 'NGO': 3}
         }
     }
 };
