9a98c016b65b608bb6f440c4b589937e62724e63
sandesh-extentia <sandesh.magdum@extentia.com>
2015-10-30 16:45:37 +0530

    - Modified user model.

--- model/users.js
+++ model/users.js
@@ -3,47 +3,52 @@ var mongoose = restful.mongoose;
 var CONSTANT = require('../utilities/Constant').CONSTANT;
 var crypto = require('crypto');
 var validator = require('validator');
-var generatePassword = require('generate-password');
-var owasp = require('owasp-password-strength-test');
+
 
 var UserSchema = new Schema({
     firstName: {
         type: String,
         trim: true,
-        default: '',
-        validate: [validateLocalStrategyProperty, 'Please fill in your first name']
+        default: ''
     },
     lastName: {
         type: String,
         trim: true,
-        default: '',
-        validate: [validateLocalStrategyProperty, 'Please fill in your last name']
+        default: ''
     },
     email: {
         type: String,
         unique: true,
         lowercase: true,
         trim: true,
-        default: '',
-        validate: [validateLocalStrategyEmail, 'Please fill a valid email address']
-    },
-    username: {
-        type: String,
-        unique: 'Username already exists',
-        required: 'Please fill in a username',
-        lowercase: true,
-        trim: true
+        default: ''
+
     },
     userrole: {
-        type: String,
-        enum: ['runner', 'ngo', 'sponsor'],
-        required: 'Please fill in a user type',
-        lowercase: true,
+        type: Number,
+        enum: [1, 2, 3],
         trim: true
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female'],
+        trim: true
+    },
+    height: {
+        type: String,
+        trim: true
+    },
+    width: {
+        type: String,
+        trim: true
+    },
+    dob: {
+        type: Date,
     },
     password: {
         type: String,
-        default: ''
+        default: '',
+        trim: true
     },
     salt: {
         type: String
@@ -77,18 +82,6 @@ UserSchema.pre('save', function (next) {
     next();
 });
 
-UserSchema.pre('validate', function (next) {
-    if (this.provider === 'local' && this.password && this.isModified('password')) {
-        var result = owasp.test(this.password);
-        if (result.errors.length) {
-            var error = result.errors.join(' ');
-            this.invalidate('password', error);
-        }
-    }
-
-    next();
-});
-
 UserSchema.methods.hashPassword = function (password) {
     if (this.salt && password) {
         return crypto.pbkdf2Sync(password, new Buffer(this.salt, 'base64'), 10000, 64).toString('base64');
@@ -116,37 +109,6 @@ UserSchema.statics.findUniqueUsername = function (username, suffix, callback) {
             }
         } else {
             callback(null);
-        }
-    });
-};
-
-UserSchema.statics.generateRandomPassphrase = function () {
-    return new Promise(function (resolve, reject) {
-        var password = '';
-        var repeatingCharacters = new RegExp('(.)\\1{2,}', 'g');
-
-        // iterate until the we have a valid passphrase. 
-        // NOTE: Should rarely iterate more than once, but we need this to ensure no repeating characters are present.
-        while (password.length < 20 || repeatingCharacters.test(password)) {
-            // build the random password
-            password = generatePassword.generate({
-                length: Math.floor(Math.random() * (20)) + 20, // randomize length between 20 and 40 characters
-                numbers: true,
-                symbols: false,
-                uppercase: true,
-                excludeSimilarCharacters: true,
-            });
-
-            // check if we need to remove any repeating characters.
-            password = password.replace(repeatingCharacters, '');
-        }
-
-        // Send the rejection back if the passphrase fails to pass the strength test
-        if (owasp.test(password).errors.length) {
-            reject(new Error('An unexpected problem occured while generating the random passphrase'));
-        } else {
-            // resolve with the validated passphrase
-            resolve(password);
         }
     });
 };
